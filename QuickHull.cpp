f9a4126c733b16dae37d8c4446d05a4355cf9bfb
Cassidy <114115152+Cassidy-Kevorkian@users.noreply.github.com>
2024-05-10 11:33:08 +0200

    Update QuickHull.cpp
    sequential quickhull implementation.

---
+++ QuickHull.cpp
@@ -0,0 +1,122 @@
+#include <iostream>
+#include <vector>
+#include "points.cpp"
+
+Point FurthestPointFromLine(Line l, std::vector<Point> points, size_t num_points) {
+    Point furthest_point = points[0];
+
+    for(int i = 1; i < num_points; ++i) {
+
+        if(dist(furthest_point, l) < dist(points[i], l)) furthest_point = points[i];
+
+    }
+    
+    return furthest_point;
+}
+
+
+std::vector<std::vector<Point>> GeneratePartition(Point p, Point q, std::vector<Point> points, size_t num_points) {
+    std::vector<std::vector<Point>> partition; 
+    std::vector<Point> part_1, part_2;
+    
+    for(int i = 0; i < num_points; ++i) {
+
+        if(((p.x == points[i].x) && (p.y == points[i].y)) || ((q.x == points[i].x) && (q.y == points[i].y))) continue;
+
+        if(cross_prod(p, q, p, points[i]) >= 0) part_1.push_back(points[i]);
+    
+        else part_2.push_back(points[i]);
+
+    }
+    
+    return {part_1, part_2};
+}
+
+
+std::vector<Point> GeneratePointsOutside(Point p, Point q, Point furthest_point, std::vector<Point> points, size_t num_points) {
+    std::vector<Point> answer;
+    
+    for(int i = 0; i < num_points; ++i) {
+        
+        if((furthest_point.x == points[i].x) && (furthest_point.y == points[i].y)) continue;
+
+        if(angle(p, q, p, furthest_point) <= angle(p, q, p, points[i])) answer.push_back(points[i]);
+
+    }
+    
+    return answer;
+}
+
+
+void QuickHullRec(Point p, Point q, std::vector<Point> points, std::vector<Point> &answer) {
+    size_t num_points = points.size();
+    
+    if(num_points == 0) return; 
+
+    Line l = generate_line(p, q);
+    Point furthest_point = FurthestPointFromLine(l, points, num_points);
+    answer.push_back(furthest_point);
+
+    std::vector<Point> points_1 = GeneratePointsOutside(p, q, furthest_point, points, num_points);
+    std::vector<Point> points_2 = GeneratePointsOutside(q, p, furthest_point, points, num_points);
+    
+    QuickHullRec(p, furthest_point, points_1, answer); 
+    QuickHullRec(q, furthest_point, points_2, answer); 
+}
+
+
+std::vector<Point> QuickHull(std::vector<Point> points) {
+    size_t num_points = points.size();
+    Point highest_point = points[0], lowest_point = points[0];
+    
+    for(int i = 1; i < num_points; ++i) {
+        
+        if(highest_point.y < points[i].y) highest_point = points[i];
+    
+        if(highest_point.y == points[i].y && highest_point.x < points[i].y) highest_point = points[i];
+    
+        if(lowest_point.y > points[i].y) lowest_point = points[i];
+ 
+        if(lowest_point.y == points[i].y && lowest_point.x > points[i].y) lowest_point = points[i];
+
+    }
+
+    std::vector<std::vector<Point>> initial_partition = GeneratePartition(lowest_point, highest_point, points, num_points);
+    std::vector<Point> points_1 = initial_partition[0], points_2 = initial_partition[1];
+    std::vector<Point> answer; 
+
+    answer.push_back(lowest_point);
+    answer.push_back(highest_point);
+
+    QuickHullRec(lowest_point, highest_point, points_1, answer);
+    QuickHullRec(lowest_point, highest_point, points_2, answer);
+    
+    return answer;
+}
+
+
+int main() {
+    std::cout << "Please enter the number of points:" << std::endl;
+    size_t num_points;
+    std::cin >> num_points;
+    std::cout << "Enter the points one by one in the  format x y:" << std::endl;
+    std::vector<Point> points; 
+    
+    for(int i = 0; i < num_points; ++i) {
+        double x, y;
+        std::cin >> x;
+        std::cin >> y;
+        points.push_back(Point(x, y));
+    }
+    
+    std::vector<Point> answer = QuickHull(points);
+    
+    std::cout << std::endl << "The points belonging to the convex hull are:" << std::endl;
+    
+    for(int i = 0; i < answer.size(); ++i) {
+        std::cout << answer[i] << std::endl;     
+    }
+
+    return 0;
+}
+
