cee43b0925ee39cdba814c47117decb970a64a33
Cassidy <114115152+Cassidy-Kevorkian@users.noreply.github.com>
2024-05-11 12:49:04 +0200

    Create QuickHullParallel.h
    QuickHullParallel header file implementation

---
+++ QuickHullParallel.h
@@ -0,0 +1,17 @@
+#ifndef QUICKHULLPARALLEL_H
+#define QUICKHULLPARALLEL_H
+#include <vector>
+#include "points.h"
+#include <thread>
+
+Point FurthestPointFromLineParallel(Line l, std::vector<Point> points, size_t num_points);
+
+std::vector<std::vector<Point>> GeneratePartitionParallel(Point p, Point q, std::vector<Point> points, size_t num_points);
+
+std::vector<Point> GeneratePointsOutsideParallel(Point p, Point q, Point furthest_point, std::vector<Point> points, size_t num_points);
+
+void QuickHullParallelRec(Point p, Point q, std::vector<Point> points, std::vector<Point> &convex_hull);
+
+std::vector<Point> QuickHullParallel(std::vector<Point> points);
+
+#endif
