da4149d06579b4b9cc80e96ec68415f92543496e
Cassidy <114115152+Cassidy-Kevorkian@users.noreply.github.com>
2024-05-11 12:48:08 +0200

    Create GrahamScan.h
    GrahamScan header implementation

---
+++ GrahamScan.h
@@ -0,0 +1,16 @@
+#ifndef GRAHAMSCAN_H
+#define GRAHAMSCAN_H
+
+#include <vector>
+#include <stack>
+#include "points.h"
+
+
+void remove_colinear(std::vector<Point> &points, Point ref);
+
+Point before_top(std::stack<Point> &stack);
+
+std::vector<Point> GrahamScan(std::vector<Point> points);
+
+
+#endif
