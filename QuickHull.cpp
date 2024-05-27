d3a1778856f4f09286e15e227b48aa0e3cbdc572
JohnnyVenturas <bogdanbudura@gmail.com>
2024-05-28 00:47:36 +0200

    formating

--- QuickHull.cpp
+++ QuickHull.cpp
@@ -1,91 +1,104 @@
+#include "QuickHull.h"
+#include "points.h"
 #include <iostream>
 #include <vector>
-#include "points.h"
-#include "QuickHull.h"
 
-Point FurthestPointFromLine(Line l, std::vector<Point> points, size_t num_points) {
+Point FurthestPointFromLine(Line l, std::vector<Point> points,
+                            size_t num_points) {
     Point furthest_point = points[0];
 
-    for(int i = 1; i < num_points; ++i) {
+    for (int i = 1; i < num_points; ++i) {
 
-        if(dist(furthest_point, l) < dist(points[i], l)) furthest_point = points[i];
+        if (dist(furthest_point, l) < dist(points[i], l))
+            furthest_point = points[i];
+    }
 
-   }
-    
     return furthest_point;
 }
 
-
-std::vector<std::vector<Point>> GeneratePartition(Point p, Point q, std::vector<Point> points, size_t num_points) {
-    std::vector<std::vector<Point>> partition; 
+std::vector<std::vector<Point>> GeneratePartition(Point p, Point q,
+                                                  std::vector<Point> points,
+                                                  size_t num_points) {
+    std::vector<std::vector<Point>> partition;
     std::vector<Point> part_1, part_2;
-    
-    for(int i = 0; i < num_points; ++i) {
 
-        if(((p.x == points[i].x) && (p.y == points[i].y)) || ((q.x == points[i].x) && (q.y == points[i].y))) continue;
+    for (int i = 0; i < num_points; ++i) {
 
-        if(cross_prod(p, q, p, points[i]) >= 0) part_1.push_back(points[i]);
-    
-        else part_2.push_back(points[i]);
+        if (((p.x == points[i].x) && (p.y == points[i].y)) ||
+            ((q.x == points[i].x) && (q.y == points[i].y)))
+            continue;
 
+        if (cross_prod(p, q, p, points[i]) >= 0)
+            part_1.push_back(points[i]);
+
+        else
+            part_2.push_back(points[i]);
     }
 
     return {part_1, part_2};
 }
 
-
-std::vector<Point> GeneratePointsOutside(Point p, Point q, Point furthest_point, std::vector<Point> points, size_t num_points) {
+std::vector<Point> GeneratePointsOutside(Point p, Point q, Point furthest_point,
+                                         std::vector<Point> points,
+                                         size_t num_points) {
     std::vector<Point> points_outside;
-    
-    for(int i = 0; i < num_points; ++i) {
-        
-        if((furthest_point.x == points[i].x) && (furthest_point.y == points[i].y)) continue;
 
-        if(angle(p, q, p, furthest_point) <= angle(p, q, p, points[i])) points_outside.push_back(points[i]);
+    for (int i = 0; i < num_points; ++i) {
 
+        if ((furthest_point.x == points[i].x) &&
+            (furthest_point.y == points[i].y))
+            continue;
+
+        if (angle(p, q, p, furthest_point) <= angle(p, q, p, points[i]))
+            points_outside.push_back(points[i]);
     }
-    
+
     return points_outside;
 }
 
-
-void QuickHullRec(Point p, Point q, std::vector<Point> points, std::vector<Point> &convex_hull) {
+void QuickHullRec(Point p, Point q, std::vector<Point> points,
+                  std::vector<Point> &convex_hull) {
     size_t num_points = points.size();
-    
-    if(num_points == 0) return; 
+
+    if (num_points == 0)
+        return;
 
     Line l = generate_line(p, q);
     Point furthest_point = FurthestPointFromLine(l, points, num_points);
     convex_hull.push_back(furthest_point);
 
-    std::vector<Point> part_1 = GeneratePointsOutside(p, q, furthest_point, points, num_points);
-    std::vector<Point> part_2 = GeneratePointsOutside(q, p, furthest_point, points, num_points);
- 
-    QuickHullRec(p, furthest_point, part_1, convex_hull); 
-    QuickHullRec(furthest_point, q, part_2, convex_hull); 
-}
+    std::vector<Point> part_1 =
+        GeneratePointsOutside(p, q, furthest_point, points, num_points);
+    std::vector<Point> part_2 =
+        GeneratePointsOutside(q, p, furthest_point, points, num_points);
 
+    QuickHullRec(p, furthest_point, part_1, convex_hull);
+    QuickHullRec(furthest_point, q, part_2, convex_hull);
+}
 
 std::vector<Point> QuickHull(std::vector<Point> points) {
     size_t num_points = points.size();
     Point highest_point = points[0], lowest_point = points[0];
-    
-    for(int i = 0; i < num_points; ++i) {
-        
-        if(highest_point.y < points[i].y) highest_point = points[i];
-    
-        if(lowest_point.y > points[i].y) lowest_point = points[i];
- 
+
+    for (int i = 0; i < num_points; ++i) {
+
+        if (highest_point.y < points[i].y)
+            highest_point = points[i];
+
+        if (lowest_point.y > points[i].y)
+            lowest_point = points[i];
     }
 
-    std::vector<std::vector<Point>> initial_partition = GeneratePartition(lowest_point, highest_point, points, num_points);
-    std::vector<Point> part_1 = initial_partition[0], part_2 = initial_partition[1];
-    std::vector<Point> convex_hull; 
+    std::vector<std::vector<Point>> initial_partition =
+        GeneratePartition(lowest_point, highest_point, points, num_points);
+    std::vector<Point> part_1 = initial_partition[0],
+                       part_2 = initial_partition[1];
+    std::vector<Point> convex_hull;
     convex_hull.push_back(lowest_point);
     convex_hull.push_back(highest_point);
-    
+
     QuickHullRec(lowest_point, highest_point, part_1, convex_hull);
     QuickHullRec(lowest_point, highest_point, part_2, convex_hull);
-    
+
     return convex_hull;
 }
--- points.cpp
+++ points.cpp
@@ -1,83 +1,84 @@
+#include "points.h"
 #include <cmath>
 #include <iostream>
-#include "points.h"
 
-std::ostream &operator<<(std::ostream &os, const Point &point)
-{
+std::ostream &operator<<(std::ostream &os, const Point &point) {
     os << "(" << point.x << ", " << point.y << ")";
     return os;
 }
 
+std::ostream &operator<<(std::ostream &os, const Line &line) {
+    if (line.a == 0 && line.c == 0)
+        os << line.b << "y = 0";
 
+    else if (line.a == 0)
+        os << line.b << "y + " << line.c << " = 0";
 
-std::ostream &operator<<(std::ostream &os, const Line &line)
-{
-    if(line.a == 0 && line.c == 0) os << line.b << "y = 0";
-    
-    else if(line.a == 0) os << line.b << "y + " << line.c << " = 0";
-    
-    else if(line.b == 0 && line.c == 0) os << line.a << "x = 0";
-    
-    else if(line.b == 0) os << line.a << "x + " << line.c << " = 0";
-        
-    else if(line.c == 0) os << line.a << "x + " << line.b << "y = 0";
-    
-    else os << line.a << "x + " << line.b << "y + " << line.c << "= 0";
+    else if (line.b == 0 && line.c == 0)
+        os << line.a << "x = 0";
+
+    else if (line.b == 0)
+        os << line.a << "x + " << line.c << " = 0";
+
+    else if (line.c == 0)
+        os << line.a << "x + " << line.b << "y = 0";
+
+    else
+        os << line.a << "x + " << line.b << "y + " << line.c << "= 0";
 
     return os;
 }
 
-double dot_prod(const Point &p1, const Point &q1, const Point &p2, const Point &q2) {
+double dot_prod(const Point &p1, const Point &q1, const Point &p2,
+                const Point &q2) {
     return (q1.x - p1.x) * (q2.x - p2.x) + (q1.y - p1.y) * (q2.y - p2.y);
 }
 
-double cross_prod(const Point &p1, const Point &q1, const Point &p2, const Point &q2) {
+double cross_prod(const Point &p1, const Point &q1, const Point &p2,
+                  const Point &q2) {
     return (q1.x - p1.x) * (q2.y - p2.y) - (q1.y - p1.y) * (q2.x - p2.x);
 }
 
-double dist(const Point &p, const Point &q)
-{
+double dist(const Point &p, const Point &q) {
     return std::sqrt((p.x - q.x) * (p.x - q.x) + (p.y - q.y) * (p.y - q.y));
 }
 
-double dist(const Point &p, const Line &l) 
-{
-   return std::abs(l.a * p.x + l.b * p.y + l.c) / std::sqrt(l.a * l.a + l.b * l.b);
+double dist(const Point &p, const Line &l) {
+    return std::abs(l.a * p.x + l.b * p.y + l.c) /
+           std::sqrt(l.a * l.a + l.b * l.b);
 }
 
-double angle(const Point &p, const Point &ref)
-{
-    if (p.x == ref.x && p.y == ref.y) 
+double angle(const Point &p, const Point &ref) {
+    if (p.x == ref.x && p.y == ref.y)
         return 0.0;
     return std::atan2(p.y - ref.y, p.x - ref.x);
 }
 
-double angle(const Point &p1, const Point &q1, const Point &p2, const Point &q2) {
+double angle(const Point &p1, const Point &q1, const Point &p2,
+             const Point &q2) {
     return std::acos(dot_prod(p1, q1, p2, q2) / (dist(p1, q1) * dist(p2, q2)));
 }
 
-bool compare_angles(const Point &p, const Point &q, const Point &ref)
-{
+bool compare_angles(const Point &p, const Point &q, const Point &ref) {
     return angle(p, ref) < angle(q, ref);
 }
 
-bool are_colinear(const Point &p, const Point &q, const Point &r)
-{
+bool are_colinear(const Point &p, const Point &q, const Point &r) {
     double tolerance = 1e-9; // Adjust tolerance as needed
-    return (std::abs(angle(p, q) - angle(p, r)) < tolerance) || std::abs(angle(p, q) + angle(p, r) - M_PI) < tolerance ;
+    return (std::abs(angle(p, q) - angle(p, r)) < tolerance) ||
+           std::abs(angle(p, q) + angle(p, r) - M_PI) < tolerance;
 }
 
-bool is_convex(const Point &p, const Point &q, const Point &r)
-{
+bool is_convex(const Point &p, const Point &q, const Point &r) {
     return (q.x - p.x) * (r.y - p.y) - (r.x - p.x) * (q.y - p.y) > 0;
 }
 
 Line generate_line(const Point &p, const Point &q) {
-    
-    if(p.x == q.x) {
+
+    if (p.x == q.x) {
         return Line(1, 0, -p.x);
     }
 
-    return Line((q.y - p.y) / (p.x - q.x), 1, (q.y * p.x - p.y * q.x) / (q.x - p.x));
-
+    return Line((q.y - p.y) / (p.x - q.x), 1,
+                (q.y * p.x - p.y * q.x) / (q.x - p.x));
 }
