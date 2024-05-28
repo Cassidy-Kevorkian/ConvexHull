9d7a8e094f31851d9c96f071d1983fa8c74b1b65
JohnnyVenturas <bogdanbudura@gmail.com>
2024-05-28 22:35:18 +0200

    Modifying tests

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,66 +1,63 @@
 // generate n concentric points on a circle and add them in tests.txt
-#include "../points.h"
 #include <cmath>
+#include <ctime>
 #include <fstream>
 #include <iostream>
+#include <points.h>
 #include <random>
-#include <ctime>
-#include "../points.h"
 
-
-//main takes two parameters n and the type of test
-void generate_tests_helper(int n, const std::string& type)
-{ //open a file to write the test cases
+// main takes two parameters n and the type of test
+void generate_tests_helper(
+    int n, const std::string &type) { // open a file to write the test cases
     double ratio;
-    if (type == "few"){
+    if (type == "few") {
         ratio = 0.1;
-    }
-    else if (type == "average"){
+    } else if (type == "average") {
         ratio = 0.5;
-    }
-    else if (type == "many"){
+    } else if (type == "many") {
         ratio = 0.9;
-    }
-    else{
+    } else {
         std::cerr << "Invalid type" << std::endl;
         return;
     }
 
     std::ofstream file("../tests/test_" + type + ".txt");
-    if (!file.is_open())
-    {
+    if (!file.is_open()) {
         std::cerr << "Error opening file" << std::endl;
         return;
-    }    
+    }
+
     file << n << std::endl;
     std::vector<Point> points;
+
     int hull_points = n * ratio;
+
     double radius = 10.0;
     double x_center = 0.0;
     double y_center = 0.0;
-    for (size_t i = 0; i < hull_points; ++i)
-    {
+
+    for (size_t i = 0; i < hull_points; ++i) {
         double x = x_center + radius * std::cos(2 * M_PI * i / hull_points);
         double y = y_center + radius * std::sin(2 * M_PI * i / hull_points);
-        //print points to file
+        // print points to file
         file << x << " " << y << std::endl;
     }
+
     radius = 8;
 
-    for (size_t i = 0; i < n - hull_points; ++i)
-    {
+    for (size_t i = 0; i < n - hull_points; ++i) {
         double angle = ((double)rand() / RAND_MAX) * 2 * M_PI;
         double rand_radius = radius * sqrt((double)rand() / RAND_MAX);
-    
+
         double x = rand_radius * cos(angle);
         double y = rand_radius * sin(angle);
-        //print points to file
+        // print points to file
         file << x << " " << y << std::endl;
     }
     file.close();
 }
 
-void generate_tests(int n){
+void generate_tests(int n) {
     srand(time(0));
 
     generate_tests_helper(n, "few");
---
+++ tests/tests.h
@@ -0,0 +1,9 @@
+#include "points.h"
+#include <fstream>
+#include <string>
+#include <unordered_set>
+namespace test {
+void check_test(const std::string &input_test, const std::string &correction);
+    std::unordered_set<Point> correction_map;
+
+}
