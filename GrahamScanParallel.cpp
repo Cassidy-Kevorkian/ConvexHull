009131c2e79668027778e502ba87a229193a4b3f
Cassidy Kevorkian-Mielly <cassidy.kevorkian@gmail.com>
2024-06-02 20:49:14 +0200

    formating

--- GrahamScanParallel.cpp
+++ GrahamScanParallel.cpp
@@ -1,147 +1,141 @@
-#include "points.h"
 #include "ParallelSorting.h"
+#include "points.h"
 #include <algorithm>
 #include <cmath>
 #include <iostream>
 #include <stack>
-#include <vector>
 #include <thread>
-
+#include <vector>
 
 namespace GrahamScanParallel {
 
-	struct UndirectedLinkedPoint {
-		UndirectedLinkedPoint(Point point) {
-			value = point;
-			next = NULL;
-			prev = NULL;
-		}
+struct UndirectedLinkedPoint {
+    UndirectedLinkedPoint(Point point) {
+        value = point;
+        next = NULL;
+        prev = NULL;
+    }
 
-		Point value;
-		UndirectedLinkedPoint *next;
-		UndirectedLinkedPoint *prev;
-	};
+    Point value;
+    UndirectedLinkedPoint *next;
+    UndirectedLinkedPoint *prev;
+};
 
+void Convexify(UndirectedLinkedPoint *begin, UndirectedLinkedPoint *end) {
 
-	void Convexify(UndirectedLinkedPoint *begin, UndirectedLinkedPoint *end) {
-	
-		//treating small cases	
-		if((begin == end) || (begin -> next == end)) {
+    // treating small cases
+    if ((begin == end) || (begin->next == end)) {
 
-			return;
-		}
+        return;
+    }
 
-		UndirectedLinkedPoint *pred = begin;
-		UndirectedLinkedPoint *curr = begin -> next;
+    UndirectedLinkedPoint *pred = begin;
+    UndirectedLinkedPoint *curr = begin->next;
 
-		while(curr != end) {
+    while (curr != end) {
 
-			UndirectedLinkedPoint *succ = curr -> next;
+        UndirectedLinkedPoint *succ = curr->next;
 
-			while(!is_convex(pred -> value, curr -> value, succ -> value)) {
-				pred -> next = succ;
-				succ -> prev = pred;
-				curr = pred;
-				pred = pred -> prev;
-			}
-				
-			pred = pred -> next;
-			curr = curr -> next;
-		}
+        while (!is_convex(pred->value, curr->value, succ->value)) {
+            pred->next = succ;
+            succ->prev = pred;
+            curr = pred;
+            pred = pred->prev;
+        }
 
-	}
-
-
-	void ConvexHullRec(UndirectedLinkedPoint *begin, UndirectedLinkedPoint *end, int num_points, const int chunk_sz) {
-
-		if(num_points > chunk_sz) {
-
-			int num_points_left = num_points / 2, 
-				num_points_right = num_points - num_points_left;
-			UndirectedLinkedPoint *mid = begin;
-
-			for(int i = 0; i < num_points_left; ++i) {
-				mid = mid -> next;
-			}
-
-			std::thread t1(&ConvexHullRec, begin, mid -> prev, num_points_left, chunk_sz);
-			std::thread t2(&ConvexHullRec, mid, end, num_points_right, chunk_sz);
-			t1.join();
-			t2.join();
-		}
-
-		Convexify(begin, end);
-	}
-
-
-	std::vector<Point> ConvexHull(std::vector<Point> &points, int NPROC) {
-		Point P = points[0];
-		int num_points = points.size();
-
-		for (int i = 0; i < num_points; ++i) {
-
-			if (points[i].y < P.y || (points[i].y == P.y && points[i].x < P.x)) {
-				P = points[i];
-			} 
-
-		}
-
-		int chunk_sz = num_points / NPROC;
-		chunk_sz = std::max(chunk_sz, 1);
-		sample_sort(points, 0, num_points, chunk_sz, P);
-		UndirectedLinkedPoint *root = new UndirectedLinkedPoint(P); // P = points[0] 
-		UndirectedLinkedPoint *iter = root;
-
-		for(int i = 1; i < num_points; ++i) { 
-			UndirectedLinkedPoint *helper = new UndirectedLinkedPoint(points[i]);
-			iter -> next = helper;
-			helper -> prev = iter;
-			iter = iter -> next;
-		}
-
-		UndirectedLinkedPoint *begin = root, *end = iter;
-
-		// we make sure the linked lists makes a full circle here
-		end -> next = begin;
-		begin -> prev = end;
-
-
-		ConvexHullRec(begin, end, num_points, chunk_sz);
-
-		iter = begin;
-		std::vector<Point> convex_hull;
-
-		while(iter != end) {
-			convex_hull.push_back(iter -> value);
-			iter = iter -> next;
-		}
-
-		convex_hull.push_back(end -> value);
-
-		return convex_hull;
-	}
-
+        pred = pred->next;
+        curr = curr->next;
+    }
 }
 
-/*
+void ConvexHullRec(UndirectedLinkedPoint *begin, UndirectedLinkedPoint *end,
+                   int num_points, const int chunk_sz) {
+
+    if (num_points > chunk_sz) {
+
+        int num_points_left = num_points / 2,
+            num_points_right = num_points - num_points_left;
+        UndirectedLinkedPoint *mid = begin;
+
+        for (int i = 0; i < num_points_left; ++i) {
+            mid = mid->next;
+        }
+
+        std::thread t1(&ConvexHullRec, begin, mid->prev, num_points_left,
+                       chunk_sz);
+        std::thread t2(&ConvexHullRec, mid, end, num_points_right, chunk_sz);
+        t1.join();
+        t2.join();
+    }
+
+    Convexify(begin, end);
+}
+
+std::vector<Point> ConvexHull(std::vector<Point> &points, int NPROC) {
+    Point P = points[0];
+    int num_points = points.size();
+
+    for (int i = 0; i < num_points; ++i) {
+
+        if (points[i].y < P.y || (points[i].y == P.y && points[i].x < P.x)) {
+            P = points[i];
+        }
+    }
+
+    int chunk_sz = num_points / NPROC;
+    chunk_sz = std::max(chunk_sz, 1);
+    sample_sort(points, 0, num_points, chunk_sz, P);
+    UndirectedLinkedPoint *root = new UndirectedLinkedPoint(P); // P = points[0]
+    UndirectedLinkedPoint *iter = root;
+
+    for (int i = 1; i < num_points; ++i) {
+        UndirectedLinkedPoint *helper = new UndirectedLinkedPoint(points[i]);
+        iter->next = helper;
+        helper->prev = iter;
+        iter = iter->next;
+    }
+
+    UndirectedLinkedPoint *begin = root, *end = iter;
+
+    // we make sure the linked lists makes a full circle here
+    end->next = begin;
+    begin->prev = end;
+
+    ConvexHullRec(begin, end, num_points, chunk_sz);
+
+    iter = begin;
+    std::vector<Point> convex_hull;
+
+    while (iter != end) {
+        convex_hull.push_back(iter->value);
+        iter = iter->next;
+    }
+
+    convex_hull.push_back(end->value);
+
+    return convex_hull;
+}
+
+} // namespace GrahamScanParallel
+
 int main() {
-	int n;
-	std::cin >> n;
-	std::vector<Point> points;
-	int NPROC = 5;
+        int n;
+        std::cin >> n;
+        std::vector<Point> points;
+        int NPROC = 5;
 
-	for(int i = 0; i < n; ++i) {
-		double a, b;
-		std::cin >> a >> b;
-		points.push_back(Point(a, b));
-	}
+        for(int i = 0; i < n; ++i) {
+                double a, b;
+                std::cin >> a >> b;
+                points.push_back(Point(a, b));
+        }
 
-	std::vector<Point> convex_hull = GrahamScanParallel::ConvexHull(points, NPROC);
+        std::vector<Point> convex_hull = GrahamScanParallel::ConvexHull(points,
+NPROC);
 
-	for(auto point: convex_hull) {
-		std::cout << point << std::endl;
-	}
+        for(auto point: convex_hull) {
+                std::cout << point << std::endl;
+        }
 
-	return 0;
+        return 0;
 }
-*/
--- ParallelSorting.cpp
+++ ParallelSorting.cpp
@@ -1,89 +1,92 @@
-#include <vector>
-#include <thread>
-#include <iostream>
-#include <algorithm>
-#include "points.h"
 #include "ParallelSorting.h"
+#include "points.h"
+#include <algorithm>
+#include <iostream>
+#include <thread>
+#include <vector>
 
 int NPROC = 2;
 
+void merge(std::vector<Point> &points, const int left, const int mid,
+           const int right, Point &P) {
+    int i = left, j = mid;
+    std::vector<Point> merged_points(right - left);
+    int k = 0;
 
-void merge(std::vector<Point> &points, const int left, const int mid, const int right, Point &P) {
-	int i = left, j = mid;	
-	std::vector<Point> merged_points(right - left);
-	int k = 0;
+    while (i < mid || j < right) {
 
-	while(i < mid || j < right) {
+        if (i == mid) {
+            merged_points[k++] = points[j];
+            ++j;
+            continue;
+        }
 
-		if(i == mid) {
-			merged_points[k++] = points[j];
-			++j;
-			continue;
-		}
+        if (j == right) {
+            merged_points[k++] = points[i];
+            ++i;
+            continue;
+        }
 
-		if(j == right) {
-			merged_points[k++] = points[i];
-			++i;
-			continue;
-		}
+        if (compare_angles(points[j], points[i], P)) {
+            merged_points[k++] = points[j];
+            ++j;
+            continue;
+        }
 
-		if(compare_angles(points[j], points[i], P)) {
-			merged_points[k++] = points[j];
-			++j;
-			continue;
-		}
-		
-		merged_points[k++] = points[i];
-		++i;
-	}
+        merged_points[k++] = points[i];
+        ++i;
+    }
 
-	for(int i = left; i < right; ++i) {
-		points[i] = merged_points[i - left];
-	}
-
+    for (int i = left; i < right; ++i) {
+        points[i] = merged_points[i - left];
+    }
 }
 
+void sample_sort(std::vector<Point> &points, int left, int right, int chunk_sz,
+                 Point &P) {
 
-void sample_sort(std::vector<Point> &points, int left, int right, int chunk_sz, Point &P) {
-	
-	if(right - left <= chunk_sz) {
-		std::sort(points.begin() + left, points.begin() + right, [&P] (Point &p1, Point &p2){ return compare_angles(p1, p2, P); });
+    if (right - left <= chunk_sz) {
+        std::sort(
+            points.begin() + left, points.begin() + right,
+            [&P](Point &p1, Point &p2) { return compare_angles(p1, p2, P); });
 
-		return;
-	}
+        return;
+    }
 
-	int mid = left + (right - left) / 2;
-	std::thread	t_left(&sample_sort, std::ref(points), left, mid, chunk_sz, std::ref(P));
-	std::thread t_right(&sample_sort, std::ref(points), mid, right, chunk_sz, std::ref(P));
+    int mid = left + (right - left) / 2;
+    std::thread t_left(&sample_sort, std::ref(points), left, mid, chunk_sz,
+                       std::ref(P));
+    std::thread t_right(&sample_sort, std::ref(points), mid, right, chunk_sz,
+                        std::ref(P));
 
-	t_left.join(), t_right.join();
+    t_left.join(), t_right.join();
 
-	merge(points, left, mid, right, P);
+    merge(points, left, mid, right, P);
 }
 
 /*
 
 int main() {
-	int n;
-	std::cin >> n;
-	std::vector<Point> points;
+        int n;
+        std::cin >> n;
+        std::vector<Point> points;
 
-	for(int i = 0; i < n; ++i) {
-		int a, b;
-		std::cin >> a >> b;
-		points.push_back(Point(a, b));
-	}
+        for(int i = 0; i < n; ++i) {
+                int a, b;
+                std::cin >> a >> b;
+                points.push_back(Point(a, b));
+        }
 
-	int chunk_sz = n / NPROC;
-	chunk_sz = std::max(chunk_sz, 1);
-	Point P = points[0];
-	sample_sort(points, 0, n, chunk_sz, P);
+        int chunk_sz = n / NPROC;
+        chunk_sz = std::max(chunk_sz, 1);
+        Point P = points[0];
+        sample_sort(points, 0, n, chunk_sz, P);
 
-	for(int i = 0; i < n; ++i) {
-		std::cout << points[i] << " ";
-	}
+        for(int i = 0; i < n; ++i) {
+                std::cout << points[i] << " ";
+        }
 
-	return 0;
+        return 0;
 }
 
 
