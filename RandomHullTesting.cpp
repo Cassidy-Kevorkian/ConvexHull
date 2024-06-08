34b334b9a50891b82df5f742a41dedb1112ea238
JohnnyVenturas <bogdanbudura@gmail.com>
2024-06-09 00:35:49 +0200

    muie

--- RandomHullTesting.cpp
+++ RandomHullTesting.cpp
@@ -1,3 +1,31 @@
 namespace random_hull_testing {  
-   
+     
+void testing_convex_hull(){
+
+}
+
+void testing_is_visible(){
+
+}
+
+void testing_build_c(){
+
+}
+
+void testing_process_ridge(){
+
+}
+
+void testing_join(){
+
+}
+
+void testing_get_min(){
+
+}
+
+void testing_process_other_ridge() {
+
+}
+
 }
